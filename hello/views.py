d27b10f7ab4f2981a7db617d6b8e1fc365021e17
Joseph Sweeney <sweeney2@purdue.edu>
2016-12-21 15:38:11 -0500

    added basic app that renders static page

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {}  # nothing dynamic to render for now
+    return render(request, 'hello/index.html', context)
+
