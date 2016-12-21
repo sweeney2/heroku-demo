fc8b3ff31ac9fe32f1342dfc50a910309d176859
Joseph Sweeney <sweeney2@purdue.edu>
2016-12-21 16:08:15 -0500

    change text, add dynamic example

--- hello/static/js/grayscale.js
+++ hello/static/js/grayscale.js
@@ -32,6 +32,13 @@ $('.navbar-collapse ul li a').click(function() {
     $(this).closest('.collapse').collapse('toggle');
 });
 
+// Reverses text as soon as it is updated
+$('#original-text').on('input', function(){
+    reversed = $(this).val().split('').reverse().join('');
+    $('#reversed-text').html(reversed.length ? reversed : '...');
+    console.log(reversed);
+});
+
 // Google Maps Scripts
 var map = null;
 // When the window has finished loading create our google map below
